567f82a5e8138d44fb098bce7c59228e0864fdae
gerald <geraldmumo6@gmail.com>
2023-05-21 14:28:52 +0300

    javacsript in project

---
+++ project.js
@@ -0,0 +1,49 @@
+const projectDone = [
+    {
+        img: "images/power_gen-20230510-0002.jpg",
+        link: "#",
+        name: "Cumminsgenerator"
+    },
+    {
+        img: "images/power_gen-20230510-0002.jpg",
+        link: "#",
+        name: "landingpage"
+    },
+    {
+        img: "images/power_gen-20230510-0002.jpg",
+        link: "#",
+        name: "aromacafe"
+    },
+    {
+        img: "images/power_gen-20230510-0002.jpg",
+        link: "#",
+        name: "essaypage"
+    },
+    {
+        img: "images/power_gen-20230510-0002.jpg",
+        link: "#",
+        name: "peterairnb"
+    }
+]
+const gridContainer = document.querySelector(".grid-container");
+
+window.addEventListener("DOMContentloaded", function(){
+    displayGridItems(projectDone);
+
+});
+
+function displayGridItems(gridItems){
+    let displayItems = gridItems.map(function(item){
+        return `<div class="proj-done">
+        <figure class="fig-img">
+            <img src=${item.img} alt=${item.name}>
+            <figcaption>
+                <a href= ${item.link}>${item.name}</a>
+            </figcaption>
+        </figure>
+         </div>`
+    });
+    displayItems = displayItems.join("");
+    gridContainer.innerHTML = displayItems;
+   
+}
